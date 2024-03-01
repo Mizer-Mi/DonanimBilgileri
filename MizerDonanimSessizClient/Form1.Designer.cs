5ecc27c1473b20ec6de1ea01e20a2d0ccc44e7a9
Mizer <mucahitizer@gmail.com>
2024-03-02 01:48:48 +0300

    project files added

---
+++ MizerDonanimSessizClient/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace MizerDonanimSessizClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HDDBOYUTTXT = new System.Windows.Forms.TextBox();
+            this.HDDMARKATXT = new System.Windows.Forms.TextBox();
+            this.RAMmarkaTXT = new System.Windows.Forms.TextBox();
+            this.AnakartModelTXT = new System.Windows.Forms.TextBox();
+            this.EkranKartiModel = new System.Windows.Forms.TextBox();
+            this.EkranKartiHiz = new System.Windows.Forms.TextBox();
+            this.CPUHIZTXT = new System.Windows.Forms.TextBox();
+            this.CPUTXT = new System.Windows.Forms.TextBox();
+            this.Isletim_SistemiTXT = new System.Windows.Forms.TextBox();
+            this.RAM_BoyutTXT = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // HDDBOYUTTXT
+            // 
+            this.HDDBOYUTTXT.Location = new System.Drawing.Point(148, 245);
+            this.HDDBOYUTTXT.Name = "HDDBOYUTTXT";
+            this.HDDBOYUTTXT.Size = new System.Drawing.Size(207, 20);
+            this.HDDBOYUTTXT.TabIndex = 1040;
+            // 
+            // HDDMARKATXT
+            // 
+            this.HDDMARKATXT.Location = new System.Drawing.Point(148, 219);
+            this.HDDMARKATXT.Name = "HDDMARKATXT";
+            this.HDDMARKATXT.Size = new System.Drawing.Size(207, 20);
+            this.HDDMARKATXT.TabIndex = 1039;
+            // 
+            // RAMmarkaTXT
+            // 
+            this.RAMmarkaTXT.Location = new System.Drawing.Point(148, 11);
+            this.RAMmarkaTXT.Name = "RAMmarkaTXT";
+            this.RAMmarkaTXT.Size = new System.Drawing.Size(207, 20);
+            this.RAMmarkaTXT.TabIndex = 1038;
+            // 
+            // AnakartModelTXT
+            // 
+            this.AnakartModelTXT.Location = new System.Drawing.Point(149, 193);
+            this.AnakartModelTXT.Name = "AnakartModelTXT";
+            this.AnakartModelTXT.Size = new System.Drawing.Size(207, 20);
+            this.AnakartModelTXT.TabIndex = 1037;
+            // 
+            // EkranKartiModel
+            // 
+            this.EkranKartiModel.Location = new System.Drawing.Point(148, 141);
+            this.EkranKartiModel.Name = "EkranKartiModel";
+            this.EkranKartiModel.Size = new System.Drawing.Size(207, 20);
+            this.EkranKartiModel.TabIndex = 1036;
+            // 
+            // EkranKartiHiz
+            // 
+            this.EkranKartiHiz.Location = new System.Drawing.Point(148, 167);
+            this.EkranKartiHiz.Name = "EkranKartiHiz";
+            this.EkranKartiHiz.Size = new System.Drawing.Size(207, 20);
+            this.EkranKartiHiz.TabIndex = 1035;
+            // 
+            // CPUHIZTXT
+            // 
+            this.CPUHIZTXT.Location = new System.Drawing.Point(148, 115);
+            this.CPUHIZTXT.Name = "CPUHIZTXT";
+            this.CPUHIZTXT.Size = new System.Drawing.Size(207, 20);
+            this.CPUHIZTXT.TabIndex = 1034;
+            // 
+            // CPUTXT
+            // 
+            this.CPUTXT.Location = new System.Drawing.Point(148, 89);
+            this.CPUTXT.Name = "CPUTXT";
+            this.CPUTXT.Size = new System.Drawing.Size(207, 20);
+            this.CPUTXT.TabIndex = 1033;
+            // 
+            // Isletim_SistemiTXT
+            // 
+            this.Isletim_SistemiTXT.Location = new System.Drawing.Point(148, 63);
+            this.Isletim_SistemiTXT.Name = "Isletim_SistemiTXT";
+            this.Isletim_SistemiTXT.Size = new System.Drawing.Size(207, 20);
+            this.Isletim_SistemiTXT.TabIndex = 1032;
+            // 
+            // RAM_BoyutTXT
+            // 
+            this.RAM_BoyutTXT.Location = new System.Drawing.Point(148, 37);
+            this.RAM_BoyutTXT.Name = "RAM_BoyutTXT";
+            this.RAM_BoyutTXT.Size = new System.Drawing.Size(207, 20);
+            this.RAM_BoyutTXT.TabIndex = 1031;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(148, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 65);
+            this.button2.TabIndex = 1030;
+            this.button2.Text = "Veri Getir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 23);
+            this.label1.TabIndex = 1041;
+            this.label1.Text = "Ram Marka:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(136, 23);
+            this.label2.TabIndex = 1042;
+            this.label2.Text = "Ram Boyut:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 23);
+            this.label3.TabIndex = 1043;
+            this.label3.Text = "İşletim Sistemi:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(136, 23);
+            this.label4.TabIndex = 1044;
+            this.label4.Text = "CPU:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(136, 23);
+            this.label5.TabIndex = 1045;
+            this.label5.Text = "CPU HIZI:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(11, 139);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(136, 23);
+            this.label6.TabIndex = 1046;
+            this.label6.Text = "Ekran Kartı Model:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(11, 167);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(136, 23);
+            this.label7.TabIndex = 1047;
+            this.label7.Text = "Ekran Kartı Hızı:";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(11, 193);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(136, 23);
+            this.label8.TabIndex = 1048;
+            this.label8.Text = "Anakart Model:";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(6, 219);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(136, 23);
+            this.label9.TabIndex = 1049;
+            this.label9.Text = "HDD Marka:";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Malgun Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(6, 245);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(136, 23);
+            this.label10.TabIndex = 1050;
+            this.label10.Text = "HDD Boyut:";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Malgun Gothic", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(236, 377);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(136, 23);
+            this.label11.TabIndex = 1051;
+            this.label11.Text = "Donanım Bilgileri Getir v.01";
+            this.label11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 409);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HDDBOYUTTXT);
+            this.Controls.Add(this.HDDMARKATXT);
+            this.Controls.Add(this.RAMmarkaTXT);
+            this.Controls.Add(this.AnakartModelTXT);
+            this.Controls.Add(this.EkranKartiModel);
+            this.Controls.Add(this.EkranKartiHiz);
+            this.Controls.Add(this.CPUHIZTXT);
+            this.Controls.Add(this.CPUTXT);
+            this.Controls.Add(this.Isletim_SistemiTXT);
+            this.Controls.Add(this.RAM_BoyutTXT);
+            this.Controls.Add(this.button2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Donanim Bilgi Getir - Mizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox HDDBOYUTTXT;
+        private System.Windows.Forms.TextBox HDDMARKATXT;
+        private System.Windows.Forms.TextBox RAMmarkaTXT;
+        private System.Windows.Forms.TextBox AnakartModelTXT;
+        private System.Windows.Forms.TextBox EkranKartiModel;
+        private System.Windows.Forms.TextBox EkranKartiHiz;
+        private System.Windows.Forms.TextBox CPUHIZTXT;
+        private System.Windows.Forms.TextBox CPUTXT;
+        private System.Windows.Forms.TextBox Isletim_SistemiTXT;
+        private System.Windows.Forms.TextBox RAM_BoyutTXT;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ MizerDonanimSessizClient/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.Devices;
+
+namespace MizerDonanimSessizClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+            Environment.Exit(0);
+        }
+
+        private static string BYTEMIZERGB(long bytes)
+        {
+            string[] Suffix = { "", "", "", "", "" };
+            int i;
+            double dblSByte = bytes;
+            for (i = 0; i < Suffix.Length && bytes >= 1024; i++, bytes /= 1024)
+            {
+                dblSByte = bytes / 1024.0;
+            }
+
+            return String.Format("{0:0.##} {1}", dblSByte, Suffix[i]);
+        }
+        private static string MHZMIZERGHZ(long bytes)
+        {
+            string[] Suffix = { "", "", "", "", "" };
+            int i;
+            Double GONDERME = (Double)bytes / (Double)1000;
+
+            return String.Format("{0:0.##} {1}", GONDERME, "");
+        }
+
+
+
+
+
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            #region İŞLEMCİ
+            string clockSpeed = "";
+            string procName = "";
+            string manufacturer = "";
+            string version = "";
+            using (ManagementObjectSearcher win32Proc = new ManagementObjectSearcher("select * from Win32_Processor"),
+    win32CompSys = new ManagementObjectSearcher("select * from Win32_ComputerSystem"),
+        win32Memory = new ManagementObjectSearcher("select * from Win32_PhysicalMemory"))
+            {
+                foreach (ManagementObject obj in win32Proc.Get())
+                {
+                    clockSpeed = obj["CurrentClockSpeed"].ToString();
+                    procName = obj["Name"].ToString();
+                    manufacturer = obj["Manufacturer"].ToString();
+                    version = obj["Version"].ToString();
+                }
+
+            }
+            #endregion
+            #region EKRAN KARTI
+            String EkranKartiModeli = "";
+            string EkranKartiBoyut = "";
+            ManagementObjectSearcher objvide = new ManagementObjectSearcher("select * from Win32_VideoController");
+
+            foreach (ManagementObject obj in objvide.Get())
+            {
+                EkranKartiModeli = obj["Name"].ToString().Trim();
+                EkranKartiBoyut = obj["AdapterRAM"].ToString().Trim();
+            }
+            EkranKartiModel.Text = EkranKartiModeli;
+            EkranKartiHiz.Text = (Math.Round(Convert.ToDouble(BYTEMIZERGB(Convert.ToInt64(EkranKartiBoyut)).Trim()), 1, MidpointRounding.AwayFromZero).ToString()) + " GB";
+
+            #endregion
+            #region Ram 
+            String Ram_Bilgileri = "";
+
+            string RAM_Model = "";
+            string RAM_HIZI = "";
+            Boolean devam = true;
+            Boolean devam2 = true;
+            string Ram_DDR_Nedir = "Bilinmiyor";
+            Ram_DDR_Nedir = MotherboardInfo.RamType;
+
+            if (Ram_DDR_Nedir.Contains("Bilinmiyor"))
+            {
+                devam2 = true;
+            }
+            else
+            {
+                devam2 = false;
+            }
+
+
+            ConnectionOptions connection = new ConnectionOptions();
+            connection.Impersonation = ImpersonationLevel.Impersonate;
+
+            ManagementScope scope = new ManagementScope("\\root\\CIMV2", connection);
+            scope.Connect();
+
+            ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_PhysicalMemory");
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+            string test = "";
+            foreach (ManagementObject queryObj in searcher.Get())
+            {
+                test = test + "-----------------------------------";
+                foreach (PropertyData data in queryObj.Properties)
+                {
+                    test = test + "\n" + data.Name + "\t" + data.Value;
+                    if (data.Name.Trim() == "PartNumber".Trim() && devam)
+                    {
+                        RAM_Model = data.Value.ToString().Trim();
+                        devam = false;
+
+
+                    }
+                    else if (data.Name.Trim() == "Speed".Trim() && devam2)
+                    {
+                        RAM_HIZI = data.Value.ToString().Trim();
+                        devam2 = false;
+
+                    }
+
+
+                }
+
+
+            }
+
+            if (Ram_DDR_Nedir.Contains("Bilinmiyor"))
+            {
+                Ram_Bilgileri = RAM_Model + " - " + RAM_HIZI + " MHz";
+            }
+            else
+            {
+                Ram_Bilgileri = RAM_Model + " - " + Ram_DDR_Nedir;
+            }
+
+
+
+
+
+            #endregion
+            #region Anakart 
+            string AnakartUretici = "";
+            if (MotherboardInfo.Manufacturer.Contains("Star International"))
+            {
+                AnakartUretici = "MSI";
+            }
+            else
+            {
+                AnakartUretici = MotherboardInfo.Manufacturer;
+            }
+            AnakartModelTXT.Text = AnakartUretici.Trim() + " - " + MotherboardInfo.Product.Trim();
+
+
+
+            #endregion
+            #region HDD
+            string HDDMarka = "";
+            long HDDBOYUTCEVIRICI = 0;
+            WqlObjectQuery q = new WqlObjectQuery("SELECT * FROM Win32_DiskDrive");
+            ManagementObjectSearcher res = new ManagementObjectSearcher(q);
+            foreach (ManagementObject o in res.Get())
+            {
+
+                HDDMarka = o["Model"].ToString().Trim();
+                break;
+
+
+            }
+            HDDMARKATXT.Text = HDDMarka;
+            foreach (System.IO.DriveInfo label in System.IO.DriveInfo.GetDrives())
+            {
+
+                if (label.IsReady)
+                {
+                    HDDBOYUTCEVIRICI = Convert.ToInt64(label.TotalSize.ToString().Trim());
+                    break;
+                }
+            }
+
+
+
+            #endregion
+
+
+            Computer MizerPc = new Computer();
+
+            string RAM = MizerPc.Info.TotalPhysicalMemory.ToString();
+            string OS = MizerPc.Info.OSFullName;
+            string CPU = procName.Trim();
+            string CPU_HIZ = (Math.Round(Convert.ToDouble(MHZMIZERGHZ(Convert.ToInt64(clockSpeed))), 2, MidpointRounding.AwayFromZero)).ToString() + " GHz";
+            CPUTXT.Text = CPU;
+            CPUHIZTXT.Text = CPU_HIZ;
+
+            Isletim_SistemiTXT.Text = OS;
+            RAMmarkaTXT.Text = Ram_Bilgileri;
+            RAM_BoyutTXT.Text = (Math.Round(Convert.ToDouble(BYTEMIZERGB(Convert.ToInt64(RAM)).Trim()), 1, MidpointRounding.AwayFromZero).ToString()) + " GB";
+            HDDBOYUTTXT.Text = (Math.Round(Convert.ToDouble(BYTEMIZERGB(Convert.ToInt64(HDDBOYUTCEVIRICI)).Trim()), 0, MidpointRounding.AwayFromZero).ToString()) + " GB";
+        }
+
+    }
+}
